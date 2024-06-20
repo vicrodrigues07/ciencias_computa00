c6845698305606dff2ece2daa407be2ea2dd27c0
vicrodrigues07 <142310764+vicrodrigues07@users.noreply.github.com>
2024-06-20 11:42:51 -0300

    Add files via upload

---
+++ ciencias_computa00-main/ciencias_computa00-main.sql
@@ -0,0 +1,21 @@
+SELECT * FROM Passagem;
+
+INSERT INTO Passagem (localEmbarque, Destino, Data, Hora, Valor, Categoria)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+INSERT INTO cliente (nome, dataNascimento, CPF, RG, Telefone, Email, Passagem_idPassagem)
+VALUES ('Larissa', '19/10/3000', '080.093.940-03', '08.098.098-0', '45 9 99259399', 'joana@gmail.com', 1);
+
+INSERT INTO cliente (nome, dataNascimento, CPF, RG, Telefone, Email, Passagem_idPassagem)
+VALUES ('Julia', '22/07/1992', '989.764.992.65', '40.776.178-4', 'julinha@gmail.com', 2);
+
+
+
+
+
+
+
+
+
+
+
